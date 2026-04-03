3380cad606b851cd3f3c22079f75448ab9962298
Marco <marco.mangan@gmail.com>
2026-04-03 04:06:11 +0000

    chore: Adding controller logging.

---
+++ server/src/main/java/org/boduha/server/RequestLoggingInterceptor.java
@@ -0,0 +1,25 @@
+package org.boduha.server;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class RequestLoggingInterceptor implements HandlerInterceptor {
+
+    private static final Logger log = LoggerFactory.getLogger(RequestLoggingInterceptor.class);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response,
+                             Object handler) {
+
+        log.info("Incoming request: {} {}", request.getMethod(), request.getRequestURI());
+
+        return true;
+    }
+}
