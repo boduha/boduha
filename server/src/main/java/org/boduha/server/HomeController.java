e1b7fa3959d0a3bef6f04885c830cf4e45d084fb
Marco <marco.mangan@gmail.com>
2026-04-03 04:05:35 +0000

    chore: Fixing white label error at /.

---
+++ server/src/main/java/org/boduha/server/HomeController.java
@@ -0,0 +1,13 @@
+package org.boduha.server;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Boduha server is running.";
+    }
+}
