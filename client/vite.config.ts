813ee59048b113cd5a1c4eaa80a135591cf6dd5f
masmangan <marco.mangan@gmail.com>
2026-04-06 13:50:39 -0300

    chore: Wifi test, CSS polishing

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,11 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
- 
+
   server: {
+    //host: true,
     proxy: {
       "/question": {
         target: "http://localhost:8080",
+
+        //target: "http://192.168.15.109:8080",
         changeOrigin: true,
       },
     },
